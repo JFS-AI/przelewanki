40d2d74a2fe2c60c415c88f24fd10a3049f03c26
Jan Franciszek Strzelecki <js480682@students.mimuw.edu.pl>
2026-01-18 00:02:26 +0100

    warKon

--- prz.cpp
+++ prz.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+int n, pojemnosc[200], koniec[200];
+
+bool czyJestJedenPelnyLubPusty() {
+	bool wynik = false;
+	for(int i = 0; i < n; i++) {
+		if(koniec[i] == 0 || pojemnosc[i] = koniec[i])
+			wynik = true;
+	}
+	return wynik;
+}
+
+bool czyWarunkiKonieczneSpelnione() {
+	return czyJestJedenPelnyLubPusty();
+}
+
 int main() {
-	int n;
 	std::cin >> n;
+
+	for(int i = 0; i < n; i++) 
+		std::cin >> pojemnosc[i] >> koniec[i];
+
+	if(!czyWarunkiKonieczneSpelnione()) {
+		std::cout << -1;
+		return 0;
+	}
+
+	
 }
